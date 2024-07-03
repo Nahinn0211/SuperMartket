3d92bfe966d1609df52980a6ba9649afa872544c
ntphuc123411 <ntphuc12341@gmail.com>
2024-07-03 17:46:32 +0700

    CommitCode1

---
+++ supermarket_frontend/src/app/Services/product.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HeaderService } from './header.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = 'http://localhost:5005/api/Products';
+
+  constructor(
+    private http: HttpClient,
+    private headerService: HeaderService
+  ) {}
+
+  private getHeaders(): HttpHeaders {
+    return this.headerService.getHeaders();
+  }
+
+  getProducts(): Observable<any[]> {
+    const headers = this.getHeaders();
+    return this.http.get<any[]>(this.apiUrl, { headers });
+  }
+
+  getProduct(id: number): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers });
+  }
+
+  createProduct(product: any): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.post<any>(this.apiUrl, product, { headers });
+  }
+
+  updateProduct(id: number, product: any): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.put<any>(`${this.apiUrl}/${id}`, product, { headers });
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.delete<any>(`${this.apiUrl}/${id}`, { headers });
+  }
+}
